40840c5bf2bf0ce6765122165f4c271b15ca985a
Erik Murray <erikjmurray@gmail.com>
2023-05-04 11:13:20 -0400

    site form verification

--- static/javascript/site_form_verification.js
+++ static/javascript/site_form_verification.js
@@ -6,6 +6,7 @@ const siteNameErrorMessage = document.getElementById('site_name_errors')
 const ipAddressInput = document.getElementById('ip_addr');
 const ipAddressErrorMessage = document.getElementById('ip_addr_errors')
 
+
 siteNameInput.addEventListener('input', function() {
     fetch(`/check_site_name?site_name=${encodeURIComponent(siteNameInput.value)}`)
     .then(response => {
@@ -19,13 +20,31 @@ siteNameInput.addEventListener('input', function() {
 })
 
 
+siteNameInput.addEventListener("change", function() {
+    if (siteNameErrorMessage.textContent) {
+            alert('Please enter a unique site name.')
+            siteNameInput.value = ''
+        }
+})
+
+
 // Uses regex to verify that IP Address is in proper format
 ipAddressInput.addEventListener("input", function() {
     if (!isValidIpAddress(ipAddressInput.value)) {
       ipAddressErrorMessage.textContent = "Please enter a valid IP address.";
+      ipAddressErrorMessage.style.backgroundColor = 'yellow'
     } else {
       ipAddressErrorMessage.textContent = "";
+      ipAddressErrorMessage.style.backgroundColor = ''
     }
+})
+
+
+ipAddressInput.addEventListener("change", function() {
+    if (!isValidIpAddress(ipAddressInput.value)) {
+        alert('Please enter a valid IP address');
+        ipAddressInput.value = '';
+        }
 })
 
 
